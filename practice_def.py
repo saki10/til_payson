0906702301c4395aef15f1ba62488f8bc5310255
saki10 <66323139+saki10@users.noreply.github.com>
2022-11-08 17:30:00 +0900

    defmemo.md

---
+++ practice_def.py
@@ -0,0 +1,15 @@
+#3の3乗は？
+
+def root(wo):
+    print(str(wo),"の10乗は",wo**3,"です")
+root(3)
+
+
+#例
+def say_hello(greeting) :
+    print(greeting)
+
+hello = say_hello
+hello("Good Morning")
+
+#==> Good Morning
